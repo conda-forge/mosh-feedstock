32096cc77e61c47c304b5b53e3da5d75e008adee
Uwe L. Korn <uwe.korn@quantco.com>
2022-11-02 13:14:33 +0100

    Remove patch; update build instructions

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+set -euxo pipefail
+
+./autogen.sh
 ./configure --prefix="${PREFIX}" \
             --with-ncurses \
             --with-curses=$PREFIX \
             --with-crypto-library=openssl \
             --disable-dependency-tracking
-mkdir include
-make version.h
-cp config.h version.h include/
-make -j${CPU_COUNT} DEFAULT_INCLUDES="-I$(pwd)/include"
-make install DEFAULT_INCLUDES="-I$(pwd)/include"
+mkdir -p include
+make -j${CPU_COUNT} DEFAULT_INCLUDES="-I$(pwd)/src/include"
+make install DEFAULT_INCLUDES="-I$(pwd)/src/include"
